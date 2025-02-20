f755bf4101bddcbe2eaaf19dcf2009021c093973
crazywolf132 <crazywolf132@gmail.com>
2025-02-20 16:39:03 +1100

    Initial Commit

---
+++ Drift/DriftApp.swift
@@ -0,0 +1,17 @@
+//
+//  DriftApp.swift
+//  Drift
+//
+//  Created by Brayden Moon on 20/2/2025.
+//
+
+import SwiftUI
+
+@main
+struct DriftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
